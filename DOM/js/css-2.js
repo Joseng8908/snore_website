f363db6fb42bdfd6fe1937ebebe76a1a8930dd6b
Jo Sangyun <106017370+Joseng8908@users.noreply.github.com>
2025-09-23 01:28:18 +0000

    css-2

---
+++ DOM/js/css-2.js
@@ -0,0 +1,9 @@
+const userName = document.querySelector("#desc p");
+
+userName.onclick = () => {
+    userName.style.color = "blue";
+    userName.style.fontSize = "30px";
+    userName.style.backgroundColor = "yellow";
+    userName.style.padding = "20px";
+    userName.style.borderRadius = "15px";
+}
