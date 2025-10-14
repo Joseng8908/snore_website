73cc16b61a89d3e703c7df5378656aabb0fff2d9
Jo Sangyun <106017370+Joseng8908@users.noreply.github.com>
2025-10-14 00:11:20 +0000

    doitjs

--- DOM/js/age.js
+++
@@ -1,9 +0,0 @@
- const currentYear = 2022;
- let birthYear;
- let age;
- birthYear = parseInt(prompt("태어난 연도를 입력하세요. (YYYY)", ""));
- age = currentYear - birthYear + 1; 
- if (age < 20) {
- alert("성인이 아닙니다. ")
- }
- alert(`${currentYear}년 현재,${age}세입니다.`);
--- DOM/js/content-2.js
+++
@@ -1,4 +0,0 @@
-const title = document.querySelector('#title');
-title.onclick = function() {
-    title.innerText = "프로필";
-}
